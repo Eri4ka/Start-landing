d3f1cc73cb8faf44c4252670d01503083a695341
Eri4ka <navyhasanov@gmail.com>
2023-03-28 23:22:15 +0500

    animation

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-
 import App from 'App';
-import 'assets/styles/globals.scss';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import 'assets/styles/globals.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
